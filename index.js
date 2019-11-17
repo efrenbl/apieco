0310cfd420e717962dde793b88ef39a6946535c0
Efren Bautista <ing.efrenbl@gmail.com>
2019-11-17 00:47:50 -0600

    adición de verbos para productos

--- index.js
+++ index.js
@@ -7,10 +7,26 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('/hola/:name', (req, res) => {
-    // res.end() Para no enviar mensaje
-    res.send({message: `Hola ${req.params.name}!`})
+app.get('/api/product', (req, res) => {
+    res.send(200, {product : []})
 })
+
+app.get('api/product/:productId',(req, res) =>{
+
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: 'El producto se ha recibido'})
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) =>{
+
+} )
 
 app.listen(port, () => {
     console.log(`API REST corriendo en http://localhost:${port}`)
