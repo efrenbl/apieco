d799ec1d4a0edf555de1b97a75d72f4eab34b639
Efren Bautista <ing.efrenbl@gmail.com>
2019-11-18 11:28:09 -0600

    refactoring middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,6 @@
 
-const jwt = require ('jwt-simple')
-const moment = require('moment')
-const config = require('../config')
+
+const services = require('../services')
 
 function isAuth (req, res, next ){
     if(!req.headers.authotization){
@@ -9,14 +8,16 @@ function isAuth (req, res, next ){
     }
 
     const token = req.headers.authotization.split(' ')[1]
-    const payload = jwt.decode(token, config.SECRET_TOKEN)
-    
-    if (payload.exp <= moment().unix()) {
-        return res.status(401).send({message: 'El token ha expirado'})
-    }
 
-    req.user = payload.sub
-    next()
+    services.decodeToken(token)
+        .then(response => {
+            req.user= response
+            next()
+        })
+        .catch(response => {
+            res.status(response.status)
+        })
+
 }
 
 module.exports = isAuth
